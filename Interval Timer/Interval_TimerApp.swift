b14d44202eb15f7bec9b29c0499170177199cd7b
Ctuch <ctuch19@students.cs.ubc.ca>
2024-01-24 12:09:54 -0800

    Initial Commit

---
+++ Interval Timer/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval Timer
+//
+//  Created by Clare Tuch on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Interval_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
