aa9070b5eb8d9e50a7f6d4057eb18d0b81554f1c
Ctuch <ctuch19@students.cs.ubc.ca>
2024-02-06 10:08:00 -0800

    Basic authentication with Spotify

--- Interval Timer/Interval_TimerApp.swift
+++ Interval Timer/Interval_TimerApp.swift
@@ -10,11 +10,18 @@ import SwiftUI
 @main
 struct Interval_TimerApp: App {
     @State private var modelData = ModelData()
+    @StateObject var spotifyController = SpotifyController()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(modelData)
+                .onOpenURL { url in
+                    spotifyController.setAccessToken(from: url)
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didFinishLaunchingNotification), perform: { _ in
+                    spotifyController.connect()
+                })
         }
     }
 }
---
+++ Interval Timer/Spotify/SpotifyController.swift
@@ -0,0 +1,102 @@
+//
+//  SpotifyController.swift
+//  Interval Timer
+//
+//  Created by Clare Tuch on 2/6/24.
+//
+
+import Foundation
+import Combine
+
+class SpotifyController: NSObject, ObservableObject, SPTAppRemotePlayerStateDelegate {
+    let spotifyClientID = "73a0abee949b47dd96c45dd8afdce012"
+    let spotifyRedirectURL = URL(string:"spotify-ios-quick-start://spotify-login-callback")!
+        
+    var accessToken: String? = nil
+    
+    var playURI = ""
+    
+    private var connectCancellable: AnyCancellable?
+    
+    private var disconnectCancellable: AnyCancellable?
+    
+    override init() {
+        super.init()
+        connectCancellable = NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                self.connect()
+            }
+        
+        disconnectCancellable = NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                self.disconnect()
+            }
+
+    }
+        
+    lazy var configuration = SPTConfiguration(
+        clientID: spotifyClientID,
+        redirectURL: spotifyRedirectURL
+    )
+
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+    
+    func setAccessToken(from url: URL) {
+        let parameters = appRemote.authorizationParameters(from: url)
+        
+        if let accessToken = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = accessToken
+            self.accessToken = accessToken
+        } else if let errorDescription = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            print(errorDescription)
+        }
+        
+    }
+    
+    func connect() {
+        guard let _ = self.appRemote.connectionParameters.accessToken else {
+            self.appRemote.authorizeAndPlayURI("")
+            return
+        }
+        
+        appRemote.connect()
+    }
+    
+    func disconnect() {
+        if appRemote.isConnected {
+            appRemote.disconnect()
+        }
+    }
+}
+
+extension SpotifyController: SPTAppRemoteDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        self.appRemote = appRemote
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+            
+        })
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("failed")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("disconnected")
+    }
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        print("state changed")
+    }
+}
